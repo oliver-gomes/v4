cb1d2d86519bb4f2bdd30db8df7c0e15eef1dcee
Oliver Gomes <ogomesphoto@gmail.com>
2019-03-09 16:03:20 -0500

    pathPrefix change

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
-  pathPrefix: "/portfolio-v4",
+  pathPrefix: "/v4",
   plugins: [
     `gatsby-plugin-transition-link`,
     `gatsby-plugin-react-helmet`,
