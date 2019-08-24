215b5d4a32f9229842911bda14b37c68f8abce0c
Oliver Gomes <ogomesphoto@gmail.com>
2019-08-24 10:32:43 -0400

    Intro change

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default class extends React.Component {
     return (
       <Layout>
         <h1>I am Oliver, nice to meet you 😄!</h1>
-        <h3>Software developer with focus on Front End Development</h3>
+        <h3>Software developer with focus on Fullstack Development</h3>
         <p>
           I love to design and develop web & mobile apps <br />
           that harmonizes the passion put behind it
